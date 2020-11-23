dd0749fb88d094b2ad8390c7323a2cc252de346a
akira.enami <tarako2533@outlook.com>
2020-11-23 17:55:09 +0000

    add user login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_103631) do
+ActiveRecord::Schema.define(version: 2020_11_22_083955) do
 
   create_table "task_lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2020_11_09_103631) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
